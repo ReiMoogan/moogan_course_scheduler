284c56f88acc07f6a95db4ec126aa89faa294834
classAndrew <classAndrew@users.noreply.github.com>
2023-09-04 16:01:43 -0700

    add building index of section meeting times

--- src/solver.rs
+++ src/solver.rs
@@ -1,91 +1,119 @@
 use std::{collections::{HashMap, HashSet}, hash::Hash, fs};
-
+use log::warn;
 use serde_json::Value;
 
 use crate::parse::{CourseListContext, SectionMeeting, MeetingType};
 
 #[derive(Debug)]
 pub struct CoursePreferences {
-    lecture_ids: Vec<u64>,
-    section_pool: Vec<SectionMeeting>,
+    // 0, 1, 2, 3 are lecture ids for any 4 classes
+    lecture_ids: Vec<u64>, 
 
-    required_discussion: HashMap<u64, bool>,
-    required_lab: HashMap<u64, bool>,
-    required_exam: HashMap<u64, bool>,
+    // indexed from 0 to whatever as 'internalized' idx for faster lookup and bitmasking
+    sections: Vec<SectionMeeting>,
+
+    // grouping by discussion and lab sections sections_idx indexed by lecture_ids' idx
+    lab_sections: Vec<Vec<usize>>,
+    lecture_sections: Vec<Vec<usize>>,
+    discussion_sections: Vec<Vec<usize>>,
+    exam_sections: Vec<Option<usize>>
 }
 
 pub struct BTSolver {
-    prefs: CoursePreferences
+    prefs: CoursePreferences,
 }
 
 impl CoursePreferences {
     pub fn new(lecture_ids: Vec<u64>, course_ctx: CourseListContext) -> Self {
-        let mut required_lab = HashMap::new();
-        let mut required_discussion = HashMap::new();
-        let mut required_exam = HashMap::new();
+        let mut lab_sections = vec![Vec::new(); lecture_ids.len()];
+        let mut discussion_sections = vec![Vec::new(); lecture_ids.len()];
+        let mut lecture_sections = vec![Vec::new(); lecture_ids.len()];
+        let mut exam_sections = vec![None; lecture_ids.len()];
 
-        let mut section_pool = course_ctx.meetings_from_lectures(&lecture_ids);
-        section_pool.sort_by_key(|section| section.u_start);
+        let mut lecture_id_to_idx: HashMap<u64, usize> = HashMap::new();
+        lecture_ids.iter().enumerate().for_each(|(i, k)| {
+            lecture_id_to_idx.insert(*k, i);
+        });
 
-        section_pool.iter().for_each(|section| {
-            required_lab.insert(section.lecture_id, *required_lab.get(&section.lecture_id).unwrap_or(&false) 
-                || matches!(section.meeting_type, MeetingType::Lab));
-            required_discussion.insert(section.lecture_id, *required_discussion.get(&section.lecture_id).unwrap_or(&false) 
-                || matches!(section.meeting_type, MeetingType::Discussion));
-            required_exam.insert(section.lecture_id, *required_exam.get(&section.lecture_id).unwrap_or(&false) 
-                || matches!(section.meeting_type, MeetingType::Exam));
+        let mut sections = course_ctx.meetings_from_lectures(&lecture_ids);
+        sections.sort_by_key(|section| section.u_start);
+
+        sections.iter().enumerate().for_each(|(idx, &section)| {
+            let lecture_idx = lecture_id_to_idx[&section.lecture_id];
+
+            match section.meeting_type {
+                MeetingType::Lecture => lecture_sections[lecture_idx].push(idx),
+                MeetingType::Discussion => discussion_sections[lecture_idx].push(idx),
+                MeetingType::Lab => lab_sections[lecture_idx].push(idx),
+                MeetingType::Exam => exam_sections[lecture_idx] = Some(idx),
+                MeetingType::Other => warn!("building course preferences index OTHER: {:?}", section),
+            };
         });
 
         Self {
             lecture_ids,
-            section_pool,
-            required_discussion,
-            required_lab,
-            required_exam
+            sections,
+
+            lab_sections,
+            lecture_sections,
+            discussion_sections,
+            exam_sections
         }
     }
 }
 
 impl BTSolver {
+    pub fn new(prefs: CoursePreferences) {
+
+    }
+
+    pub fn solve(&self) {
+
+    }
+
+    fn backtrack(&self, schedule_mask: &Vec<bool>) {
+        
+    }
+
     // assume schedule is always sorted by start date
     // assume at least one lecture session for all desired lectures
-    pub fn score(&self, schedule: &Vec<SectionMeeting>) -> f64 {
-        // check any intersections and count lectures and discussions
-        let mut ending = 0; // pretty sure no classes are on sunday 12am
-        let mut seen_lab = HashSet::new();
-        let mut seen_discussion = HashSet::new();
+    // fn score(&self, schedule: &Vec<SectionMeeting>) -> f64 {
+    //     // check any intersections and count lectures and discussions
+    //     let mut ending = 0; // pretty sure no classes are on sunday 12am
+    //     let mut seen_lab = HashSet::new();
+    //     let mut seen_discussion = HashSet::new();
     
-        for i in 0..schedule.len() {
-            if schedule[i].u_start <= ending {
-                return -1.0; // a "soft rejection" preferred in GASolver. penalize by overlap in time
-            }
+    //     for i in 0..schedule.len() {
+    //         if schedule[i].u_start <= ending {
+    //             return -1.0; // a "soft rejection" preferred in GASolver. penalize by overlap in time
+    //         }
 
-            let lecture_id = schedule[i].lecture_id;
+    //         let lecture_id = schedule[i].lecture_id;
 
-            let is_lab = matches!(schedule[i].meeting_type, MeetingType::Lab);
-            if is_lab && seen_lab.contains(&lecture_id) { return -1.0; } // makes no sense to have 2 lab sections
+    //         let is_lab = matches!(schedule[i].meeting_type, MeetingType::Lab);
+    //         if is_lab && seen_lab.contains(&lecture_id) { return -1.0; } // makes no sense to have 2 lab sections
 
-            let is_discussion = matches!(schedule[i].meeting_type, MeetingType::Discussion);
-            if is_discussion && seen_discussion.contains(&lecture_id) { return -1.0; } // makes no sense to have 2 discussion sections
+    //         let is_discussion = matches!(schedule[i].meeting_type, MeetingType::Discussion);
+    //         if is_discussion && seen_discussion.contains(&lecture_id) { return -1.0; } // makes no sense to have 2 discussion sections
 
-            if is_lab { seen_lab.insert(lecture_id); }
-            else if is_discussion { seen_discussion.insert(lecture_id); }
+    //         if is_lab { seen_lab.insert(lecture_id); }
+    //         else if is_discussion { seen_discussion.insert(lecture_id); }
 
-            ending = schedule[i].u_end;
-        }
+    //         ending = schedule[i].u_end;
+    //     }
  
-        // check all the lectures have disc/lab if they require them
-        if self.prefs.lecture_ids.iter().all(|lecture_id| {
-            let req_discussion = self.prefs.required_discussion[lecture_id];
-            let req_lab = self.prefs.required_lab[lecture_id];
-            (!req_discussion || (req_discussion && seen_discussion.contains(lecture_id))) &&
-            (!req_lab || (req_lab && seen_discussion.contains(lecture_id)))
-        }) {
-            return -1.0; // GASolver "soft rejection" penalizes by the number of missing sections
-        }
+    //     // check all the lectures have disc/lab if they require them
+    //     if self.prefs.lecture_ids.iter().all(|lecture_id| {
+    //         let req_discussion = self.prefs.required_discussion[lecture_id];
+    //         let req_lab = self.prefs.required_lab[lecture_id];
+    //         (!req_discussion || (req_discussion && seen_discussion.contains(lecture_id))) &&
+    //         (!req_lab || (req_lab && seen_discussion.contains(lecture_id)))
+    //     }) {
+    //         return -1.0; // GASolver "soft rejection" penalizes by the number of missing sections
+    //     }
 
-        1.0 // again, "encouraging approval" preferred in GA solver, incentivizing "good" schedules like no late classes
-    }
+    //     1.0 // again, "encouraging approval" preferred in GA solver, incentivizing "good" schedules like no late classes
+    // }
 }
 
 #[test]
